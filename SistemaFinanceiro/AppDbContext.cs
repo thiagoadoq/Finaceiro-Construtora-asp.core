2ea7426418cf3d85268575448338428f5d794138
thiago alves de oliveira <thiagoadoq@gmail.com>
2019-10-29 13:20:12 -0300

    Subundo aplicação

---
+++ SistemaFinanceiro/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaFinanceiro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaFinanceiro
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+        public DbSet<Terceiros> Terceiros { get; set; }
+
+        public DbSet<Financeriros> Financeriros { get; set; }
+
+    }
+}
